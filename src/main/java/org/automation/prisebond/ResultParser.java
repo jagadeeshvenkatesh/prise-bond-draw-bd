18e47af6815a99a6751471ecc719f1434119db80
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-17 23:02:48 +0600

    generic parser

---
+++ src/main/java/org/automation/prisebond/ResultParser.java
@@ -0,0 +1,13 @@
+package org.automation.prisebond;
+
+import org.jsoup.nodes.Document;
+
+/**
+ * Created by shantonu on 5/17/2021
+ */
+public class ResultParser {
+    public static String parse(Document doc){
+        String result = "";
+        return result;
+    }
+}
