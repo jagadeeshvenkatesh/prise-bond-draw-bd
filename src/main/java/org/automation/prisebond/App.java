0cdf5df97c9052dfee02f8f7e33004d54784de07
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-17 22:22:52 +0600

    Executable Class

---
+++ src/main/java/org/automation/prisebond/App.java
@@ -0,0 +1,19 @@
+package org.automation.prisebond;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * Created by shantonu on 5/16/2021
+ * https://www.bb.org.bd/investfacility/prizebond/searchPbond.php?txtNumbers=0437535
+ */
+public class App {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        List<String> ids = BondNoReader.read();
+        for(String id : ids){
+            System.out.println(id+" : >>> : "+ResultRequest.check(id));
+        }
+    }
+}
