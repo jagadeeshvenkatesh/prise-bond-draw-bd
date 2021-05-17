fc5d16bfbaf8aa0fdee6b233540c218bfbc19a93
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-17 22:36:50 +0600

    JSOUP completed

--- src/main/java/org/automation/prisebond/ResultRequest.java
+++ src/main/java/org/automation/prisebond/ResultRequest.java
@@ -15,19 +15,21 @@ import java.net.URL;
  */
 public class ResultRequest {
     private static Document doc;
+    private static String no;
     public static String BASE_URL="www.bb.org.bd";
     public static String PATH="/investfacility/prizebond/searchPbond.php";
     public static String USER_AGENT_CHROME="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36";
     public static String ACCEPT= "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
     public static String ACCEPT_ENCODING= "gzip, deflate, br";
+
     public static String check(final String prise_bond_no) throws IOException {
         URL url = new URL("https://"+BASE_URL+PATH+"?txtNumbers="+prise_bond_no);
         doc = Jsoup.parse(url,3000);
         Element message = doc.select(".asteriskRed").first();
-
+        no=prise_bond_no;
         return processResults(message.wholeText());
     }
-    public static String processResults(final String status){
+    private static String processResults(final String status){
         if(status.contains("No match")){
             return status;
         }
@@ -35,9 +37,12 @@ public class ResultRequest {
             Element table= doc.select("table").first();
             Elements rows = table.select("tr");
             String result = "";
-            for(Element row : rows){
-                result = row.
+            for (int i = 1; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                result = "!!!Congratulation!!!, "+no+" NO has won "+cols.get(2).text()+" of BDT "+cols.get(3).text();
             }
+
             return result;
         }
     }
