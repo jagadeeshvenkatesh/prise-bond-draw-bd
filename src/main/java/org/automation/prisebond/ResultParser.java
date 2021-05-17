e3eb8eb299e9d7250cf0e1920f19dd902f384b41
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-17 23:13:37 +0600

    generic parser

--- src/main/java/org/automation/prisebond/ResultParser.java
+++ src/main/java/org/automation/prisebond/ResultParser.java
@@ -1,6 +1,13 @@
 package org.automation.prisebond;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 /**
  * Created by shantonu on 5/17/2021
@@ -8,6 +15,26 @@ import org.jsoup.nodes.Document;
 public class ResultParser {
     public static String parse(Document doc){
         String result = "";
-        return result;
+        Element message = doc.select(".asteriskRed").first();
+        String status = message.wholeText();
+        if(status.contains("No match")){
+            return status;
+        }
+        else {
+            Element table= doc.select("table").first();
+            Elements rows = table.select("tr");
+            for (int i = 1; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                result = "!!!Congratulation!!!, "+cols.get(0).text()+" NO has won "+cols.get(2).text()+" of BDT "+cols.get(3).text();
+            }
+            return result;
+        }
+
     }
+    public static String parse(HttpEntity entity) throws IOException {
+        return parse(Jsoup.parse(EntityUtils.toString(entity)));
+    }
+
+
 }
