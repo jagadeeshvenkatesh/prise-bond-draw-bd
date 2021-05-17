949e6b515a16b87a29e7950c14d5200bf2910d6f
shantonu sarker <sarker.shantonu@gmail.com>
2021-05-17 23:14:02 +0600

    central properties

---
+++ src/main/java/org/automation/prisebond/AppProperties.java
@@ -0,0 +1,12 @@
+package org.automation.prisebond;
+
+/**
+ * Created by shantonu on 5/17/2021
+ */
+public class AppProperties {
+    public static String BASE_URL="www.bb.org.bd";
+    public static String PATH="/investfacility/prizebond/searchPbond.php";
+    public static String USER_AGENT_CHROME="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36";
+    public static String ACCEPT= "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+    public static String ACCEPT_ENCODING= "gzip, deflate, br";
+}
